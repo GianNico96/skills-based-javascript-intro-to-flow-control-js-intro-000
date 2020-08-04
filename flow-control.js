1637c75b99647d803244ee0661722ad05e7ae647
looping-torrent-8105 <gianherrera96@gmail.com>
2020-08-04 03:55:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,8 +27,8 @@ if (age >= 13 && age <= 19) {
 }
 
 function ternaryTeenager(age) {
-  return age >= 13 && age <= 19 ? 
-  "You are a teenager!" : 
+  return age >= 13 && age <= 19 ?
+  "You are a teenager!" :
   "You are not a teenager"
 }
 
